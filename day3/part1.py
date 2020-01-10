ccda0a4b5f87a7e999d1f7f23716045e34ba7809
sharkbound <laptopblaster@gmail.com>
2020-01-09 21:31:55 -0600

    refactor

--- day3/part1.py
+++ day3/part1.py
@@ -3,13 +3,12 @@ from itertools import permutations
 from read import read_lines
 
 
+def is_valid(lengths):
+    return all(x + y > z for x, y, z in permutations(lengths))
+
+
 def solve(data):
-    print(
-        sum(
-            all(x + y > z for x, y, z in permutations(lengths))
-            for lengths in data
-        )
-    )
+    print(sum(map(is_valid, data)))
 
 
 def main():
