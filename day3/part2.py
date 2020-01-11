c1ba7665df77b6a949643a591145fdf819b171a9
sharkbound <laptopblaster@gmail.com>
2020-01-10 19:40:24 -0600

    small refactor

--- day3/part2.py
+++ day3/part2.py
@@ -4,12 +4,14 @@ import numpy as np
 
 
 def solve(data: np.ndarray):
-    total = 0
-    for i in range(3):
-        for lengths in data[..., i].reshape((-1, 3)):
-            if all(x + y > z for x, y, z in permutations(lengths)):
-                total += 1
-    print(total)
+    print(
+        sum(
+            1
+            for i in range(3)
+            for lengths in data[..., i].reshape((-1, 3))
+            if all(x + y > z for x, y, z in permutations(lengths))
+        )
+    )
 
 
 def main():
